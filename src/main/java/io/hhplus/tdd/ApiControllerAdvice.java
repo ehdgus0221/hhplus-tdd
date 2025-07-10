374482f8679fe86e56f8d3a2dc615a8863a41924
DongHyeon Lim <dlaehdgus23@naver.com>
2025-07-11 03:53:48 +0900

    feat : 애플리케이션 레이어 - 포인트 관련 에러 처리 코드 작성

--- src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
+++ src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
@@ -1,5 +1,7 @@
 package io.hhplus.tdd;
 
+import io.hhplus.tdd.exception.MaxChargeAmountException;
+import io.hhplus.tdd.exception.MinChargeAmountException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,6 +9,34 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @RestControllerAdvice
 class ApiControllerAdvice extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(MinChargeAmountException.class)
+    public ResponseEntity<ErrorResponse> handleMinCharge(MinChargeAmountException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse("MIN_CHARGE_ERROR", "최소 충전 금액은 100원입니다"));
+    }
+
+    @ExceptionHandler(MaxChargeAmountException.class)
+    public ResponseEntity<ErrorResponse> handleMaxCharge(MaxChargeAmountException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse("MAX_CHARGE_ERROR", "최대 충전 금액은 100만원입니다"));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse("ILLEGAL_ARGUMENT", ex.getMessage()));
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalState(IllegalStateException ex) {
+        return ResponseEntity
+                .status(409)  // Conflict
+                .body(new ErrorResponse("INSUFFICIENT_BALANCE", ex.getMessage()));
+    }
+
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         return ResponseEntity.status(500).body(new ErrorResponse("500", "에러가 발생했습니다."));
